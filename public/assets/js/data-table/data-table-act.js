6e39e05db04eea565a6beb85f304a2a8f795327b
Kate <kate.bolanos@indigo21.com>
2024-07-01 16:53:54 +0800

    DataTables

--- public/assets/js/data-table/data-table-act.js
+++ public/assets/js/data-table/data-table-act.js
@@ -2,7 +2,32 @@
  "use strict";
 	
 	$(document).ready(function() {
-		 $('#data-table-basic').DataTable();
+		 $('#data-table-basic').DataTable({
+			"paging": true,
+			"lengthChange": true,
+			"searching": true,
+			"ordering": true,
+			"info": true,
+			"autoWidth": false,
+			"responsive": true,
+		 });
+		 $('#data-table-order-report').DataTable({
+			"paging": true,
+			"lengthChange": true,
+			"searching": false,
+			"ordering": true,
+			"info": true,
+			"autoWidth": false,
+			"responsive": true,
+			dom: 'lBfrtip',
+			buttons: [
+				{ extend: 'copy', className: 'btn btn-primary glyphicon glyphicon-duplicate' },
+				{ extend: 'csv', className: 'btn btn-primary glyphicon glyphicon-save-file' },
+				{ extend: 'excel', className: 'btn btn-primary glyphicon glyphicon-list-alt' },
+				{ extend: 'pdf', className: 'btn btn-primary glyphicon glyphicon-file' },
+				{ extend: 'print', className: 'btn btn-primary glyphicon glyphicon-print' }
+			]
+		 });
 	});
- 
 })(jQuery); 
+
