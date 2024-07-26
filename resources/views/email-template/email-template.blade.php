0cb951f33733a0dd270b4c4a6fe4c76aafb77e33
Kate <kate.bolanos@indigo21.com>
2024-07-26 21:32:34 +0800

    Email Template

---
+++ resources/views/email-template/email-template.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Invoice</title>
+        <style type="text/css">
+            body{
+                font-family: 'open sans', 'helvetica neue', sans-serif;
+            }
+            table {
+                width: 100%;
+            }
+            .header {
+                background: #8981d8;
+            }
+            .header h1 {
+                color:#fff;
+                text-align: center;
+                font-family: 'open sans', 'helvetica neue', sans-serif;
+            }
+            .content {
+                background: #fff;
+            }
+            .content td, .signature td {
+                padding: 5px 40px; 
+            }
+        </style>
+    </head>
+    <body>
+        <table>
+            <tr class="header">
+                <td>
+                    <h1>Fingal Memorials</h1>
+                </td>
+            </tr>
+            <tr class="content">
+                <td>
+                    <h3>Dear Ms. Name,</h3>
+                    <p>Please find attached details of your order.</p>
+                    <p>If you have any questions, please do not hesitate to contact us.</p>
+                </td>
+            </tr>
+            <tr class="signature">
+                <td>
+                    <p><b>Kind Regards,</b></p>
+                    <p><b>Fingal Memorials</b></p>
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
