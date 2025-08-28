d51e8023287f15de902022c13c1cd6c0027cb8fb
Charles-indigo21 <charles.verdadero@indigo21.com>
2025-08-28 19:11:33 +0800

    Update order-report.js

--- public/js/order-report.js
+++ public/js/order-report.js
@@ -1,22 +1,8 @@
 const SYSTEM_URL = $("body").attr("url");
 $(document).ready(function() {
-    $('#data-table-order-report').DataTable({
-        "paging": true,
-        "lengthChange": true,
-        "searching": false,
-        "ordering": true,
-        "info": true,
-        "autoWidth": false,
-        "responsive": true,
-        dom: 'lBfrtip',
-        buttons: [
-            { extend: 'copy', className: 'btn btn-primary glyphicon glyphicon-duplicate' },
-            { extend: 'csv', className: 'btn btn-primary glyphicon glyphicon-save-file' },
-            { extend: 'excel', className: 'btn btn-primary glyphicon glyphicon-list-alt' },
-            { extend: 'pdf', className: 'btn btn-primary glyphicon glyphicon-file' },
-            { extend: 'print', className: 'btn btn-primary glyphicon glyphicon-print' }
-        ]
-     });
+    
+    initDataTables();
+
     $(document).on('click', '#searchOrder', function() {
         let startDate = $('input[name="order_date_start"]').val();
         let endDate = $('input[name="order_date_end"]').val();
@@ -45,23 +31,49 @@ $(document).ready(function() {
                     
                 </tr>
                 `;  
-                data.map((orderData, index) => {
-                    let splitDate   = orderData.order_date.split("-");
-                    tableRow += `
-                    <tr>
-                        <td>${orderData.id}</td>
-                        <td>${splitDate[2]}/${splitDate[1]}/${splitDate[0]}</td>
-                        <td>${orderData.branch_name}</td>
-                        <td>${orderData.order_type_name}</td>
-                        <td>${orderData.order_headline}</td>
-                        <td>${orderData.customer_name}</td>
-                        <td>${orderData.value || "0"}</td>
-                        <td>${orderData.balance || "0"}</td>
-                    </tr>`;
-                })
-               
+                if(data.length > 0){
+                    tableRow = "";
+                    data.map((orderData, index) => {
+                        let splitDate   = orderData.order_date.split("-");
+                        tableRow += `
+                        <tr>
+                            <td>${orderData.id}</td>
+                            <td>${splitDate[2]}/${splitDate[1]}/${splitDate[0]}</td>
+                            <td>${orderData.branch_name}</td>
+                            <td>${orderData.order_type_name}</td>
+                            <td>${orderData.order_headline}</td>
+                            <td>${orderData.customer_name}</td>
+                            <td>${orderData.value || "0"}</td>
+                            <td>${orderData.balance || "0"}</td>
+                        </tr>`;
+                    });
+                }
+                
                 $("#orderReportData").html(tableRow);
+                initDataTables();
             }
         })
     });
+
+    function initDataTables(){
+        $('#data-table-order-report').DataTable().destroy();
+
+        $('#data-table-order-report').DataTable({
+            "paging": true,
+            "lengthChange": true,
+            "searching": false,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "responsive": true,
+            dom: 'lBfrtip',
+            buttons: [
+                { extend: 'copy', className: 'btn btn-primary glyphicon glyphicon-duplicate' },
+                { extend: 'csv', className: 'btn btn-primary glyphicon glyphicon-save-file' },
+                { extend: 'excel', className: 'btn btn-primary glyphicon glyphicon-list-alt' },
+                { extend: 'pdf', className: 'btn btn-primary glyphicon glyphicon-file' },
+                { extend: 'print', className: 'btn btn-primary glyphicon glyphicon-print' }
+            ]
+        });
+    }
 });
