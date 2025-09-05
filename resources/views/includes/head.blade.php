0a406969820d83f3e8f19132a897177f34cc6534
Charles-indigo21 <charles.verdadero@indigo21.com>
2025-09-05 18:04:39 +0800

    Update head.blade.php

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>| FINGAL MEMORIALS LTD</title>
+    <title>FINGAL MEMORIALS LTD</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
